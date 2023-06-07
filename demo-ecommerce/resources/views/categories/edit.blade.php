dde9089e3b19701798beb0bd4ded844fadb3e13e
kashyappatha <95681551+kashyappatha@users.noreply.github.com>
2023-06-07 19:27:43 +0530

    commit edit.blade.php

--- demo-ecommerce/resources/views/categories/edit.blade.php
+++ demo-ecommerce/resources/views/categories/edit.blade.php
@@ -4,7 +4,7 @@
     <h1 class="mb-0">Edit category</h1><br />
 
     <hr />
-    <form action="{{ route('categories.update', $category->id) }}" method="POST">
+    <form action="{{ route('categories.update', $category->id) }}" method="POST" id="kp">
         @csrf
         @method('PUT')
         <input type="hidden" name="id" value="{{ $category->id }}">
@@ -15,22 +15,20 @@
                     value="{{ $category->category }}">
             </div>
 
+
             <div class="row">
                 <div class="col mb-3">
                     <label class="form-label">Image:</label>
-                    <input type="file" name="image"
-                        class="form-control"accept="image/jpeg, image/png , image/jpg, image/svg" placeholder="Enter Image">
+                    <input type="file" name="image" class="form-control"
+                        accept="image/jpeg, image/png, image/jpg, image/svg" placeholder="Enter Image">
                     @if ($category->image)
                         <img src="{{ asset('admin_assets/img/' . $category->image) }}" alt="Image"
-                            style="max-width:60px;" accept="image/jpeg, image/png , image/jpg">
+                            style="max-width:60px;" accept="image/jpeg, image/png, image/jpg">
                         @if ($category->image)
                             <div class="mt-2">
-
-                                <label class="form-check-label">
-                                    <input type="submit" name="image" value="Delete"class="form-check-input"
-                                        onclick="confirmDelete()">
-
-                                </label>
+                                <button type="button" class="btn btn-danger" onclick="confirmDelete()">
+                                    <i class="fas fa-trash"></i> Delete
+                                </button>
                             </div>
                         @endif
                     @endif
@@ -47,19 +45,30 @@
                                 cancelButtonText: 'Cancel'
                             }).then((result) => {
                                 if (result.isConfirmed) {
-                                    // Proceed with the delete action
-                                    alert('Image deleted!');
-                                    // Place your delete logic here
+                                    // Delete the image from the server
+                                    deleteImage();
                                 }
                             });
                         }
+
+                        function deleteImage() {
+                            // Send an AJAX request to delete the image
+                            axios.delete('{{ route('categories.deleteImage', $category->id) }}')
+                                .then((response) => {
+                                    if (response.data.success) {
+                                        alert('Image deleted!');
+                                        // Reload the page or perform any other necessary action
+                                    } else {
+                                        alert('Failed to delete image!');
+                                    }
+                                })
+                                .catch((error) => {
+                                    alert('An error occurred while deleting the image!');
+                                    console.error(error);
+                                });
+                        }
                     </script>
-
-
-
-
                 </div>
-                <script></script>
                 <div class="col mb-3">
                     <label class="form-label">Status:</label>
                     <div>
@@ -105,52 +114,16 @@
                         return true;
                     }
 
-                    document.getElementById('yourFormId').addEventListener('submit', function(event) {
+                    document.getElementById('kp').addEventListener('submit', function(event) {
                         if (!validateForm()) {
                             event.preventDefault();
                         }
                     });
                 </script>
-                {{-- <div class="col mb-3">
-                    <label class="form-label">Available:</label>
-                    <select name="available" id="available-select" class="form-control" value="{{ $category->available }}"
-                        onchange="showSelectedCategory(this)">
-                        <option value="">Select Availibilty Type:</option>
-                        <option value="available">Available</option>
-                        <option value="unavailable">unavailable</option>
-                    </select>
-                    <input type="hidden" name="available_value" id="available-value" value="">
-                </div>
-                <script>
-                    function showSelectedCategory(selectElement) {
-                        var selectedCategory = selectElement.value;
-                        var selectedTitle = "{{ $category->id }}";
-
-                        Swal.fire({
-                            title: 'Selected Category',
-                            text: 'You have chosen ' + selectedCategory + ' for the product with title: ' + selectedTitle,
-                            icon: 'info',
-                            timer: 3000,
-                            timerProgressBar: true,
-                            showConfirmButton: false
-                        });
-                    }
-                    document.getElementById('available-select').addEventListener('change', function() {
-                        var selectedOption = this.value;
-                        var statusValue = (selectedOption === '1') ? 'Available' : 'unavailable';
-                        document.getElementById('available-value').value = statusValue;
-                    });
-                </script>
- --}}
-
 
 
 
             </div>
-
-
-
-
         </div>
         <div class="row">
             <div class="d-grid">
@@ -164,4 +137,6 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+
 @endsection
